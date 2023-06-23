46ebee26fae1467c54a89d10b2e0fcabf88d67a9
Hiro Ayettey <hiro.ayettey@gmail.com>
2023-06-23 01:58:57 -0400

    gpt help

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { Stone } from './stone.svelte';
 	import Square from './square.svelte';
+
 	// Initialize grid to empty
 	let gridSize = 19;
 	let grid: Array<Array<number>> = [];
@@ -16,115 +17,50 @@
 	let stonesPlaced = 0;
 	let stoneLimit = 1;
 
-	let movesThisTurn: Array<Array<number>> = [];
-	function undoTurn() {
-		for (let i = 0; i < movesThisTurn.length; i++) {
-			grid[movesThisTurn[i][0]][movesThisTurn[i][1]] = Stone.None;
-		}
-		stonesPlaced = 0;
+	let socket: WebSocket;
+
+	if (typeof window !== 'undefined') {
+		// Create WebSocket connection
+		socket = new WebSocket('ws://localhost:3000');
+
+		// Connection opened
+		socket.addEventListener('open', (event) => {
+			socket.send('Hello Server!');
+		});
+
+		// Listen for messages
+		socket.addEventListener('message', (event) => {
+			console.log('Message from server ', event.data);
+			const gameState = JSON.parse(event.data);
+			grid = gameState.grid;
+			turn = gameState.turn;
+			stonesPlaced = gameState.stonesPlaced;
+			stoneLimit = gameState.stoneLimit;
+		});
+	}
+	async function undoTurn() {
+		await fetch('http://localhost:3000/undoTurn');
 	}
 
-	function resetGame() {
-		for (let i = 0; i < gridSize; i++) {
-			let arr = [];
-			for (let j = 0; j < gridSize; j++) {
-				arr.push(Stone.None);
-			}
-			grid[i] = arr;
-		}
-		turn = Stone.Black;
-		stonesPlaced = 0;
-		stoneLimit = 1;
-	}
-	function confirm() {
-		if (stonesPlaced === stoneLimit && turn === Stone.Black) {
-			turn = Stone.White;
-			stonesPlaced = 0;
-			movesThisTurn = [];
-		}
-
-		stoneLimit = 2;
-
-		if (stonesPlaced === stoneLimit && turn === Stone.White) {
-			turn = Stone.Black;
-			stonesPlaced = 0;
-			movesThisTurn = [];
-		}
+	async function resetGame() {
+		await fetch('http://localhost:3000/resetGame');
 	}
 
-	function checkWin(row: number, col: number) {
-		const directions = [
-			[
-				[0, 1],
-				[0, -1]
-			],
-			[
-				[1, 0],
-				[-1, 0]
-			],
-			[
-				[1, 1],
-				[-1, -1]
-			],
-			[
-				[-1, 1],
-				[1, -1]
-			]
-		];
-
-		for (let i = 0; i < directions.length; i++) {
-			let dx1 = directions[i][0][0],
-				dy1 = directions[i][0][1];
-			let dx2 = directions[i][1][0],
-				dy2 = directions[i][1][1];
-			let total = 1;
-
-			// Check in the first direction
-			let mult = 1;
-			while (
-				0 <= row + dx1 * mult &&
-				row + dx1 * mult < gridSize &&
-				0 <= col + dy1 * mult &&
-				col + dy1 * mult < gridSize &&
-				grid[row + dx1 * mult][col + dy1 * mult] === turn
-			) {
-				total += 1;
-				mult += 1;
-			}
-
-			// Check in the second direction
-			mult = 1;
-			while (
-				0 <= row + dx2 * mult &&
-				row + dx2 * mult < gridSize &&
-				0 <= col + dy2 * mult &&
-				col + dy2 * mult < gridSize &&
-				grid[row + dx2 * mult][col + dy2 * mult] === turn
-			) {
-				total += 1;
-				mult += 1;
-			}
-
-			// Check if we have a win
-			if (total >= 6) {
-				alert(turn === Stone.Black ? 'Black wins' : 'White wins');
-				break;
-			}
+	async function confirm() {
+		if (stonesPlaced == stoneLimit) {
+			await fetch('http://localhost:3000/confirm');
 		}
 	}
 
-	function onSquareClick(i: number, j: number) {
+	async function onSquareClick(i: number, j: number) {
 		if (grid[i][j] !== Stone.None || stonesPlaced === stoneLimit) {
 			return;
 		}
 
-		console.log(turn, stonesPlaced);
-
 		stonesPlaced += 1;
 		grid[i][j] = turn;
-		movesThisTurn.push([i, j]);
 
-		checkWin(i, j);
+		await fetch(`http://localhost:3000/placeStone?i=${i}&j=${j}`);
 	}
 </script>
 
@@ -141,7 +77,7 @@
 	<div class="grid">
 		{#each grid as line, i}
 			{#each line as square, j}
-				<Square stone={square} onClick={() => onSquareClick(i, j)} />
+				<Square stone={square} {i} {j} onClick={() => onSquareClick(i, j)} />
 			{/each}
 		{/each}
 	</div>
--- src/routes/square.svelte
+++ src/routes/square.svelte
@@ -2,14 +2,22 @@
 	import { Stone } from './stone.svelte';
 	export let stone: Object;
 	export let onClick: any;
+	export let i: number;
+	export let j: number;
+
+	// Determine if the square is a star point
+	let starPoint = (i === 3 || i === 9 || i === 15) && (j === 3 || j === 9 || j === 15);
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div class="container" on:click={onClick}>
 	<div class="inner">
-		<div class="line horizontal" />
-		<div class="line vertical" />
+		<div class="line horizontal" class:top={i === 0} class:bottom={i === 18} />
+		<div class="line vertical" class:left={j === 0} class:right={j === 18} />
 		<div class:white={stone === Stone.White} class:black={stone === Stone.Black} />
+		{#if starPoint}
+			<div class="star-point" />
+		{/if}
 	</div>
 </div>
 
@@ -19,7 +27,7 @@
 		justify-content: center;
 		align-items: center;
 		margin: 0;
-		background-color: bisque;
+		background-color: #dda15e; /* Change to a wooden color */
 	}
 
 	.inner {
@@ -40,6 +48,16 @@
 		height: 1px;
 	}
 
+	.horizontal.top {
+		left: 50%;
+		width: 100%;
+	}
+
+	.horizontal.bottom {
+		left: 10%;
+		width: 100%;
+	}
+
 	.vertical {
 		left: 50%;
 		top: 0;
@@ -47,24 +65,43 @@
 		height: 100%;
 	}
 
-	.white {
-		height: 95%;
-		width: 95%;
+	.vertical.left {
+		top: 50%;
+		height: 100%;
+	}
+
+	.vertical.right {
+		top: 50%;
+		height: 100%;
+	}
+
+	.white,
+	.black {
+		height: 80%;
+		width: 80%;
 		position: absolute;
 		top: 50%;
 		left: 50%;
-		background-color: white;
 		border-radius: 50%;
 		transform: translate(-50%, -50%);
+		box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+	}
+
+	.white {
+		background-color: #f3f3f3; /* Lighter color for white stone */
 	}
 
 	.black {
-		height: 95%;
-		width: 95%;
+		background-color: #111; /* Darker color for black stone */
+	}
+
+	.star-point {
+		height: 10px;
+		width: 10px;
 		position: absolute;
 		top: 50%;
 		left: 50%;
-		background-color: black;
+		background-color: #000;
 		border-radius: 50%;
 		transform: translate(-50%, -50%);
 	}
